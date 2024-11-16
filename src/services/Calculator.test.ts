bc54f7cecbfe6f848d8d58c00f47d2681a94ec01
nyx-desdev <nyx.desdev@gmail.com>
2024-11-16 21:32:23 +0530

    2 numbers and newlines between numbers

--- src/services/Calculator.test.ts
+++ src/services/Calculator.test.ts
@@ -12,7 +12,15 @@ describe("Calculator", () => {
     expect(calculator.add("")).toBe(0);
   });
 
-  test('single number returns the number', () => {
-    expect(calculator.add('1')).toBe(1);
+  test("single number returns the number", () => {
+    expect(calculator.add("1")).toBe(1);
+  });
+
+  test("two numbers returns their sum", () => {
+    expect(calculator.add("1,2")).toBe(3);
+  });
+
+  test("handles newlines between numbers", () => {
+    expect(calculator.add("1\n2,3")).toBe(6);
   });
 });
--- src/services/Calculator.ts
+++ src/services/Calculator.ts
@@ -2,7 +2,7 @@ export class Calculator {
   add(numbers: string): number {
     if (!numbers) return 0;
     return numbers
-      .split(",")
+      .split(/[\s,]+/)
       .map(Number)
       .reduce((acc, num) => acc + num, 0);
   }
